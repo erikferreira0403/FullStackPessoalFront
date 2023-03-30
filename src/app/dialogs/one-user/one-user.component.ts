69a80646fd7fcfa7f0c8468d9d23bbcfc980e158
Erik Alves <erikalves.alu.lmb@gmail.com>
2023-03-30 18:02:39 -0300

    improve edit user feature

--- src/app/dialogs/one-user/one-user.component.ts
+++ src/app/dialogs/one-user/one-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user.service';
 
@@ -8,25 +9,32 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./one-user.component.css']
 })
 export class OneUserComponent implements OnInit {
-  
+
   @Input() id!: number;
   disclaimer = false;
+
+  public showPassword: boolean = false;
+
 
   public users : User = {
     name: '',
     password: ''
     };
-  constructor(private user : UserService) { }
+  constructor(private user : UserService, @Inject(MAT_DIALOG_DATA) public data: {id: number}) { }
 
   ngOnInit(): void {
-  }
+    this.OneUser();
+    }
   OneUser(){
-    this.user.GetOneUser(this.id).subscribe(r => this.users = r)
+    this.user.GetOneUser(this.data.id).subscribe(r => this.users = r)
   }
   EditUser(){
     if(this.users.name != "" && this.users.password != ""){
 
-    this.user.PutUser(this.id, this.users).subscribe(() =>
+    this.user.PutUser(this.data.id, this.users).subscribe(() =>
     window.location.reload())} else {  this.disclaimer = true
-    } 
+    }
+  }
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }}
